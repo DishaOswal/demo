d7e01e301e1489a512be4f902f8f19a5ec4848a7
DishaOswal <dishamoswal@gmail.com>
2018-11-29 16:46:27 +0530

    Create example.java

---
+++ example.java
@@ -0,0 +1,10 @@
+public class App 
+{
+    public static void main( String[] args )
+    {
+        for(int i=1;i<=20;i++) {
+        
+    	System.out.println( "Hello World from devops!"+i);
+      }
+    }     
+}
